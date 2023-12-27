b441f499aabd0ed80f4639148daceb79553897ac
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2023-12-27 22:18:56 +0300

    added codes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './Footer'
+import footer_logo from '../Assets/shopping-bag.svg'
+
+const Footer = () => {
+    return (
+        <div className="footer"> 
+        <div className="footer-logo">
+        <img src={footer_logo} alt=""/>
+        <p>STYLE</p>
+        </div>
+        <ul className="footer-links">
+          <li>Company</li>
+          <li>Products</li>
+          <li>Offices</li>
+          <li>About</li>
+          <li>Contact</li>  
+        </ul>
+        <div className="footer-social-icon">
+          <div className="footer-icons-container">
+          <i class='bx bxl-instagram'></i>
+          <i class='bx bxl-telegram'></i>
+          <i class='bx bxl-whatsapp'></i>
+          </div>
+        </div>
+        <div className="footer-copyright">
+            <hr/>
+            <p>Copyright @ 2023 - All Right Reversed.</p>
+        </div>
+        </div>
+    )
+}
+
+export default Footer
