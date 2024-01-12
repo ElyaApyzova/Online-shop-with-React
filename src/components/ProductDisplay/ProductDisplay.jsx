024e885dca0682ec219a6502c50e65570fc54393
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-01-12 23:21:07 +0300

    breadcrumbs

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -20,7 +20,7 @@ const ProductDisplay = (props) => {
       </div>
       <div className='productdisplay-right'>
         <h1>{product.name}</h1>
-        <div className='productdisplay-right-star'>
+        <div className='productdisplay-right-stars'>
             <img src={star_icon} alt=''/>
             <img src={star_icon} alt=''/>
             <img src={star_icon} alt=''/>
@@ -35,9 +35,9 @@ const ProductDisplay = (props) => {
         <div className='productdisplay-right-description'>
           A lightweight, usually knitted, pullover shirt, close-fitting and a round neckline and short sleeves, worn as an undershirt.   
         </div>
-        <div className='productdisplay-right-size'>
+        <div className='productdisplay-right-sizes'>
             <h1>Select Size</h1>
-            <div className='productdisplay-right-size'>
+            <div className='productdisplay-right-sizes'>
                <div>S</div>
                <div>M</div>
                <div>L</div>
