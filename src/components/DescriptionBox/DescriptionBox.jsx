e4e07beb0f897445bee6ee11fcd88318a7834a98
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-01-13 23:18:51 +0300

    Products

---
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+        <div className='descriptionbox-navigator'>
+            <div className='descriptionbox-nav-box'>Description</div>
+            <div className='descriptionbox-nav-box fade'>Reviews (122)</div>
+        </div>
+        <div className='descriptionbox-description'>
+        An e-commerce has played an important role in the fashion and apparel industry. Discover the statistics, trends, and strategies shaping online sales. 
+        <p>eCommerce refers to the buy and sell transactions of fashion and apparel products online, specifically through eCommerce platforms</p> 
+        </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
