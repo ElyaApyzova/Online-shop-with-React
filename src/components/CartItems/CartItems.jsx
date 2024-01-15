f03115a79b33387d42dfaaa20f3b7f42ad98008b
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-01-15 23:47:27 +0300

    added

--- src/components/CartItems/CartItems.jsx
+++ src/components/CartItems/CartItems.jsx
@@ -23,13 +23,13 @@ const CartItems = () => {
            if(CartItems[e.id]>0) 
     {
       return <div>
-      <div className='cartitems-format'>
+      <div className='cartitems-format cartitems-format-main'>
        <img src={e.image}  alt='' className='carticon-product-icon'/>
        <p>{e.name}</p>
        <p>{e.new_price}</p>
        <button className='cartitems-quantity'>{CartItems[e.id]}</button>
        <p>{e.new_price*CartItems[e.id]}</p>
-       <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt=''/>
+       <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt=''/>
       </div>
       <hr/>
       </div>
